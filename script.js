528b86ecb546aeafdb44c72b4bc438ca0060688c
azedmedia <abdulazeemismail88@gmail.com>
2025-12-28 20:58:52 -0300

    ok

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
  function goToWhatsapp() {
     window.location.href="https://chat.whatsapp.com/K51SSvo6D7c0G284QvbbUj"
 }
+function goToWhatsapp() {
+    const confirmJoin = confirm("Do you want to Join crypto with anonymous?");
+    if (confirm) {
+         window.location.href="https://chat.whatsapp.com/K51SSvo6D7c0G284QvbbUj"
+    }
+}
