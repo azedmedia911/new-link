ffaa2c3a481818195a7f59cb90a3423f168e493e
azedmedia <abdulazeemismail88@gmail.com>
2025-12-28 23:46:37 -0300

    yes

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
  function goToWhatsapp() {
     window.location.href="https://chat.whatsapp.com/K51SSvo6D7c0G284QvbbUj"
-    
 }
 function goToWhatsapp() {
     const confirmJoin = confirm ("Do you want to join with anonymous?")
     if (confirmJoin) {
         window.location.href="https://chat.whatsapp.com/K51SSvo6D7c0G284QvbbUj"
-    }
-    else {
+    } else {
         console.log("you choose not to join");
-    } 
-}
-    
+    } }
+
+   
