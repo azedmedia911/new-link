c0d60d6109001c758de4667fea90983ea460e039
azedmedia911 <abdulazeemismail1@gmail.com>
2025-12-29 23:06:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,4 +11,31 @@ function goToWhatsapp() {
         console.log("you choose not to join");
     } 
 }
-    
+
+
+   function updateDateTime() {
+  const now = new Date();
+
+  let year = now.getFullYear();
+  let month = now.toLocaleString('default', { month: 'long' });
+  let day = now.getDate();
+
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+
+  let ampm = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+
+  let dateString = `year{month} day`;
+  let timeString = `{hours}:minutes:{seconds} ampm`;
+
+  document.getElementById('datetime').textContent = `{dateString} | ${timeString}`;
+}
+
+setInterval(updateDateTime, 1000);
+updateDateTime(); 
